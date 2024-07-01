6562a4967e4b1493517dd65609e29b0e069ca8f0
riverroman <3169233891503@ingenieria.usac.edu.gt>
2024-07-01 11:53:56 -0600

    Commit Final

--- Analyzer/src/Ast/NodoAst.java
+++ Analyzer/src/Ast/NodoAst.java
@@ -6,12 +6,12 @@ import java.util.List;
 public class NodoAst {
     private String valor;
     private List<NodoAst> listaHijos;
-
+    
     public NodoAst(String valor) {
         this.valor = valor;
         this.listaHijos = new ArrayList<>();
     }
-
+    
     public void agregarHijo(String val) {
         listaHijos.add(new NodoAst(val));
     }
@@ -21,11 +21,11 @@ public class NodoAst {
             listaHijos.add(hijo);
         }
     }
-
+    
     public String getValor() {
         return valor;
     }
-
+    
     public List<NodoAst> getListaHijos() {
         return listaHijos;
     }
